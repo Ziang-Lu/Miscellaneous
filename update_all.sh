05988cdc640562be5f1385d36b460490c6c2b566
Ziang Lu <ziangl@alumni.cmu.edu>
2020-06-04 10:13:45 +0800

    Add update_all.sh

---
+++ update_all.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+FOLDERS=$(ls)
+for FOLDER in $FOLDERS
+    do
+        if [ -d $FOLDER ]; then
+            echo "Entering and updating $FOLDER"
+            cd $FOLDER
+            git pull
+            cd ..
+        fi
+done
